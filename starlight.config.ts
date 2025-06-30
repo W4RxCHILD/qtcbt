365362b06a2882d238555539059cc23d85a08e38
w4rxchild <w4rxchild@gmail.com>
2025-06-30 05:53:18 -0600

    Initial push of QuickTurn CBT site

---
+++ starlight.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@astrojs/starlight/config';
+
+export default defineConfig({
+  title: 'QuickTurn CBT',
+  sidebar: [
+    {
+      label: 'All CBTs',
+      items: [
+        { label: 'Maintenance Cyber Discipline', link: '/docs/maintenance-cyber-discipline' },
+        { label: 'Test2', link: '/docs/test2' },
+        { label: 'Test3', link: '/docs/test3' },
+        { label: 'Test4', link: '/docs/test4' },
+        { label: 'Test5', link: '/docs/test5' },
+        { label: 'Test6', link: '/docs/test6' },
+        { label: 'Test7', link: '/docs/test7' },
+      ]
+    }
+  ]
+});
