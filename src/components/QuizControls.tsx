62ee745a25ee39df325043292f5d6522f0b227d5
w4rxchild <w4rxchild@gmail.com>
2025-07-15 06:20:26 -0600

    I need caffiene

--- src/components/QuizControls.tsx
+++ src/components/QuizControls.tsx
@@ -1,23 +1,31 @@
 export default function QuizControls() {
-  const sharedButtonClasses = `w-56 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white 
+  const sharedButtonClasses = `px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white 
     dark:bg-blue-300 dark:hover:bg-blue-200 dark:text-black 
-    font-semibold rounded-md shadow border border-white dark:border-black transition`;
+    font-semibold rounded-md shadow border border-white dark:border-black transition 
+    text-center`;
 
   return (
     <div className="sticky top-4 z-10 flex justify-center">
-      <div className="flex flex-row gap-2">
-        <button
-          onClick={() => window.dispatchEvent(new Event('revealAllQuestions'))}
-          className={sharedButtonClasses}
-        >
-          Check All
-        </button>
-        <button
-          onClick={() => window.dispatchEvent(new Event('resetAllQuestions'))}
-          className={sharedButtonClasses}
-        >
-          Reset
-        </button>
+      <div className="inline-flex gap-4">
+        {/* Check All Button defines width & height */}
+        <div className="w-56 h-12">
+          <button
+            onClick={() => window.dispatchEvent(new Event('revealAllQuestions'))}
+            className={`${sharedButtonClasses} w-full h-full`}
+          >
+            Check All
+          </button>
+        </div>
+
+        {/* Reset Button auto-sizes to match */}
+        <div className="h-12">
+          <button
+            onClick={() => window.dispatchEvent(new Event('resetAllQuestions'))}
+            className={`${sharedButtonClasses} h-full px-6`}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
